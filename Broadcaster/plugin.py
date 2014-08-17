19de9fa6d0222d817607180c96e0b395f70a4880
gorbinphilip <gorbinphilip@gmail.com>
2014-08-17 14:45:59 +0530

    adding abstract class Plugin

---
+++ Broadcaster/plugin.py
@@ -0,0 +1,18 @@
+class Plugin(object):
+    """Provides abstract class for loaded plugins of each sites"""
+
+    def __init__(self,msg):
+        """Constructor for Plugin class. The msg is the content to be posted to the site like tweet for twitter etc"""
+        raise NotImplementedError()
+
+    def post(self):
+        """Method to invoke plugin to post message to site"""
+        raise NotImplementedError()
+
+    def status(self):
+        """Method to query status of the plugin activity"""
+        raise NotImplementedError()
+
+    def force_exit(self):
+        """This method should kill the plugin activity"""
+        raise NotImplementedError()
