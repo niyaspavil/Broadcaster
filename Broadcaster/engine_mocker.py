e30e10c8026b3089b68da2677cff7439ff9fe85a
gorbinphilip <gorbinphilip@gmail.com>
2014-08-18 17:30:22 +0530

    added engine_mocker for temporary key access

---
+++ Broadcaster/engine_mocker.py
@@ -0,0 +1,14 @@
+import ConfigParser
+
+class Engine(object):
+    """Mocker class for engine"""
+    
+    def __init__(self):
+        """nothing special... :P"""
+        self.plugin="twitter"
+
+    def get_attrib(self, option):
+        """return attribute from conf.ini"""
+        config=ConfigParser.ConfigParser()
+        config.read("conf.ini")
+        return config.get(self.plugin, option)
