8cae8aa776f030d44b91c1f5ab65448551d4667d
gorbinphilip <gorbinphilip@gmail.com>
2014-08-18 20:15:44 +0530

    added test file for twitter.py

---
+++ tests/test_twitter.py
@@ -0,0 +1,9 @@
+from Broadcaster import twitter
+
+def test_twitter_plugin(msg="The funny dummy tweet~!!.."):
+    """test for twitter plugin"""
+    tmp_plug=twitter.Tweet(msg)
+    assert tmp_plug.post==True
+
+test_twitter_plugin()
+test_twitter_plugin("The real tweet!!!")
