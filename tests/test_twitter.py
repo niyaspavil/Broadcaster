38efa16912e63383116f6ebcfcaed9aaf617e7f1
gorbinphilip <gorbinphilip@gmail.com>
2014-08-18 20:40:47 +0530

    corrected test_twitter to call twitter methods instead of referring it

--- tests/test_twitter.py
+++ tests/test_twitter.py
@@ -3,7 +3,7 @@ from Broadcaster import twitter
 def test_twitter_plugin(msg="The funny dummy tweet~!!.."):
     """test for twitter plugin"""
     tmp_plug=twitter.Tweet(msg)
-    assert tmp_plug.post==True
+    assert tmp_plug.post()==True
 
 test_twitter_plugin()
 test_twitter_plugin("The real tweet!!!")
