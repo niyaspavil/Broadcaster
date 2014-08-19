0c18227b7f221fc5113ea247de90cc657d62dbf5
niyaspavil <pavilniyas@gmail.com>
2014-08-19 13:30:29 +0530

    tested for empty message and string

---
+++ tests/test_ui.py
@@ -0,0 +1,8 @@
+
+from Broadcaster import Ui
+
+test =Ui.Terminal_Ui()
+f=test.get_message_and_channel()
+
+print f
+
--- tests/tst_ui.py
+++
@@ -1,9 +0,0 @@
-
-from Ui import Terminal_Ui
-
-test = Terminal_Ui()
-f=test.get_message_and_channel()
-print f
-
-
-
