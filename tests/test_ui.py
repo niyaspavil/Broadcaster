e7b0933676fdf30688f3056695272ff6bda64286
niyaspavil <pavilniyas@gmail.com>
2014-08-20 10:22:53 +0530

    added inavalid calling of engine whene input is invalid

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -5,7 +5,7 @@ from Broadcaster import Mock_engin
 
 test =Ui.Terminal_Ui()
 f=test.get_message_and_channel()
+if f:
+    k=Mock_engin.engin(f)
 
-k=Mock_engin.engin(f)
-
-print k.get_response()
+    print k.get_response()
