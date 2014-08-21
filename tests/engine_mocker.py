ea0f76f4a23bc0d72057250bbb8a91a902319040
gorbinphilip <gorbinphilip@gmail.com>
2014-08-21 15:36:58 +0530

    added some more funcs to engine_mocker

--- tests/engine_mocker.py
+++ tests/engine_mocker.py
@@ -1,4 +1,5 @@
-import ConfigParser
+
+conf = {'consumer_key':'BCxhHxyTR4vHtKXqa7jSTicv4', 'cosumer_secret':'sCpiNzjR9MTcFHbVWkuukm0ucKbYXnyv1ZdZQpDlRXAyzSMfpy', 'user_token':'2736495205-8f8yRTQeQ7JFhQHsqfSPWtl2iDafsJiiPsymOW1', 'user_token_secret':'vbzYBj1AbX4FZrrDtT2caQCVPt16Di0LTvsVArXTjQ3Ht'}
 
 class Engine(object):
     """Mocker class for engine"""
@@ -8,7 +9,13 @@ class Engine(object):
         self.plugin="twitter"
 
     def get_attrib(self, option):
-        """return attribute from conf.ini"""
-        config=ConfigParser.ConfigParser()
-        config.read("conf.ini")
-        return config.get(self.plugin, option)
+        """return attribute from conf"""
+        return conf.get(option)
+
+    def set_attrib(self, option, value):
+        """stores option-value pair to the conf"""
+        conf[option]=value
+
+    def prompt_user(self, msg):
+        """prompts user with msg and return the input from user"""
+        return 999
