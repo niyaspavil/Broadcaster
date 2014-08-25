96f29a9a38099611f53d5440bc7f7b7ca086672c
gorbinphilip <gorbinphilip@gmail.com>
2014-08-25 17:41:35 +0530

    added Access_Token class to tweepy

--- tests/tweepy.py
+++ tests/tweepy.py
@@ -1,8 +1,16 @@
 
-class OAuthHandler():
+class OAuthHandler(object):
 
    def __init__(self, cs_key=None, cs_secret=None):
       pass
 
    def get_authorization_url(self):
       return "test_url"
+
+   def get_access_token(self, pin):
+      self.access_token=self.Access_Token()
+
+   class Access_Token(object):
+      def __init__(self):
+         self.key="user999"
+         self.secret="user999"
