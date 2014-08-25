bd009174faa28ef39afc2be42cf0f8819f6e4fbf
gorbinphilip <gorbinphilip@gmail.com>
2014-08-25 17:56:35 +0530

    included set_access_token to tweepy

--- tests/tweepy.py
+++ tests/tweepy.py
@@ -2,15 +2,22 @@
 class OAuthHandler(object):
 
    def __init__(self, cs_key=None, cs_secret=None):
-      pass
+      self.access_token=self.Access_Token()
 
    def get_authorization_url(self):
       return "test_url"
 
    def get_access_token(self, pin):
-      self.access_token=self.Access_Token()
+      self.access_token.set_access("user999", "user999")
+
+   def set_access_token(self, usr_key, usr_secret):
+      self.access_token.set_access(usr_key, usr_secret)
 
    class Access_Token(object):
       def __init__(self):
-         self.key="user999"
-         self.secret="user999"
+         self.key=""
+         self.secret=""
+
+      def set_access(self, key, secret):
+         self.key=key
+         self.secret=secret
