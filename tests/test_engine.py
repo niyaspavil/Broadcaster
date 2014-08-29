6d0e38d49244d9f4a80ccc54c24f2a114edea819
niyaspavil <pavilniyas@gmail.com>
2014-08-29 23:55:50 +0530

    added ui object in broadcaste function

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,12 +1,13 @@
 from ..Broadcaster import dummy_engine
 from ..tests.plugin_mocker import plugin_mocker
-
+from ..tests.mock_ui import Mock_ui
 
 load_plugin=dummy_engine.load_plugin
 
 def test_broadcast():
+    ui=Mock_ui()
     dummy_engine.load_plugin=plugin_mocker
-    dummy_engine.broadcast("testing", ['twitter'])
+    dummy_engine.broadcast("testing", ['twitter'],ui)
 
 def test_load_plugin():
-    load_plugin("twitter","test")
+    load_plugin("twitter","test",)
