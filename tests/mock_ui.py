ead7e5ce5659e671460475b0dd41468abf369090
niyaspavil <pavilniyas@gmail.com>
2014-08-29 23:58:27 +0530

    added mock file for ui

---
+++ tests/mock_ui.py
@@ -0,0 +1,22 @@
+from ..Broadcaster import ui
+class Mock_ui():
+	 
+    def __init__(self):
+    
+        pass
+    
+    def get_mesg_and_chanl():
+        
+        pass
+
+    def display_status(self,status):
+        
+       pass
+
+    def display_error(self,error):
+        
+       pass
+
+    def prompt(self,content):
+	
+	pass
