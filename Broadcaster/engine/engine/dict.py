25d54d391f051d1e3c7af05a5e48e6285d3b015d
vinayak <vinayak.nitc@gmail.com>
2014-08-29 10:16:36 +0530

    Removed old files with hard coding and added a ini file

--- Broadcaster/engine/engine/dict.py
+++
@@ -1,7 +0,0 @@
-# Funtion to convert list of channels given by user ie ul to a dictionary such that each of channel inputed by user maps to it's actual channel name.
-def dict (ul):
-    from decode import decode
-    d = {}
-    for i in ul:
-        d [i] = decode (i)
-    return d
