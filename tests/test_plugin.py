d269234da47caf71c7a8ec9fd2eb76f38a4aed2b
gorbinphilip <gorbinphilip@gmail.com>
2014-09-02 10:46:36 +0530

    fixed test for plugin

---
+++ tests/test_plugin.py
@@ -0,0 +1,26 @@
+from ..Broadcaster.plugin import Plugin
+
+class tester_plugin(Plugin):
+    def __init__(self, msg):
+        pass
+try:
+    base_plugin=Plugin("failure")
+except NotImplementedError:
+    pass
+
+plugin_test_obj=tester_plugin("error_test")
+
+try:
+    plugin_test_obj.post()
+except NotImplementedError:
+    pass
+
+try:
+    plugin_test_obj.status()
+except NotImplementedError:
+    pass
+
+try:
+    plugin_test_obj.force_exit()
+except NotImplementedError:
+    pass
