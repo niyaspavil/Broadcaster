ce922bf4ac07b5b1cc5940ce1565ba58cffb0978
niyaspavil <pavilniyas@gmail.com>
2014-09-04 22:47:02 +0530

    removed some bugs in engine on mail

--- tests/test_mail.py
+++ tests/test_mail.py
@@ -7,6 +7,7 @@ from ..Broadcaster.plugin import PluginError
 
 msg=''.join(random.choice(string.lowercase) for x in range(10))
 mail.smtplib = smtplib_mocker
+mail.Engine= Engine
 tmp_plug=mail.mail(msg)
 tmp_engine=Engine("mail")
 tmp_plug.engine=tmp_engine	
@@ -14,7 +15,7 @@ tmp_engine.mock_input="consumer999"
 
 
 def test_init():
-    """test for twitter plugin initialisation"""
+    """test for mail plugin initialisation"""
 
     assert tmp_plug.state=="waiting"
     assert tmp_plug.msg==msg
@@ -25,21 +26,16 @@ def test_status():
     assert tmp_plug.status()=="waiting"
 
 def test_get_consumer_details():
-    """test for get_consumer_keys method"""
+    """test for get_consumer_details method"""
   
     assert tmp_plug.get_consumer_details()==['consumer999','consumer999']
     assert tmp_plug.get_consumer_details()==['consumer999','consumer999']
 
 def test_pre_authenicate():
-    """test for get_user_keys method"""
-    #mail.smptplib = smtplib_mocker
+    """test for pre_authenticate method"""
     assert tmp_plug.pre_authenticate()==True
 
 def test_post():
     """test for post method"""
     tmp_plug.server = smtplib_mocker.SMTP() 
     assert tmp_plug.post()==True
-
-#def test_real():
-#    tmp_plug=twitter.twitter(msg)
-#    assert tmp_plug.post()==True
