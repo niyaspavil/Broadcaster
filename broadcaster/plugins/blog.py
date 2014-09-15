c0a09f63439a0eb57d8ec8883679f11b1d19d982
vinayak <vinayak.nitc@gmail.com>
2014-09-15 23:40:22 +0530

    Pre authenticate function to get username, password and other details

--- broadcaster/plugins/blog.py
+++ broadcaster/plugins/blog.py
@@ -16,3 +16,20 @@ class blog():
             self.engine=Engine()
         except Exception:
             raise PluginError(PluginError.ERROR)
+
+    def pre_authenticate(self):
+        """This method create a server objct and user names and password"""
+        wp_url=self.engine.get_attrib('wp_url',self.name)
+        if wp_url=='' or wp_url==None:
+            self.state="waiting for wordpress url detials"
+            wp_url=self.engine.prompt_user("Enter wordpress url", str)
+            self.engine.set_attrib('wp_url',wp_url,self.name)
+        wp_url  = wp_url+"/xmlrpc.php"
+        try:
+            self.server = xmlrpclib.ServerProxy(wp_url)
+        except:
+            print "server error"
+        user_name,user_password=self.get_consumer_details()
+        self.username = user_name
+        self.password = user_password
+        return True 
