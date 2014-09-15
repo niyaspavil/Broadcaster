4a3d8540f0f363da35cb6d95fb6cfd6325c9b7e2
vinayak <vinayak.nitc@gmail.com>
2014-09-15 22:51:41 +0530

    Added a mock xmlrpc to test blog

---
+++ broadcaster/test/xmlrpc_mocker.py
@@ -0,0 +1,12 @@
+class ServerProxy():
+
+    class metaWeblog():
+
+        def newPost():
+            return "first post"
+
+        def getCategories(x, username, password):
+            return "categorys' list"
+
+def DateTime():
+    return "today's date"
