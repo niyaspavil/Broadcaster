3aa15227d6bea9976def89e9c059ae18ba2a3b6e
vinayak <vinayak.nitc@gmail.com>
2014-09-16 10:53:47 +0530

    Consumer details tested sucsessfully

--- broadcaster/test/test_blog.py
+++ broadcaster/test/test_blog.py
@@ -14,12 +14,14 @@ tmp_plug.engine.mock_input="consumer999"
 def test_init():
     assert tmp_plug.state=="waiting"
     assert tmp_plug.msg==msg
+
+def test_customer_details():
+    assert tmp_plug.get_consumer_details() == ["consumer999","consumer999"]
+
 """
 def test_pre_authenticate():
     assert tmp_plug.pre_authenticate() == True
-    
-def test_customer_details():
-    assert tmp_plug.customer_details() == ["vinayak","password"]
+
 
 def test_compose_post():
     assert post = {
