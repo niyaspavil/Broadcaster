09b6715c3d59c4da7d0530d44f277b0fc0df8b9c
niyaspavil <pavilniyas@gmail.com>
2014-09-24 13:30:20 +0530

    bug fixed in setup.py

--- broadcaster.py
+++
@@ -1,3 +0,0 @@
-from broadcaster import terminal_ui
-import sys
-terminal_ui.main(sys.argv[1:])
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Broadcaster',
+    name='python-broadcaster',
     version='1.0.0',
     author='Gorbin, Niyas, Vinayak',
     author_email='broadcaster.thelycaeum@gmail.com',
