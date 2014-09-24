c11f58e9d006db6841067ee9f238308beddfd794
niyaspavil <pavilniyas@gmail.com>
2014-09-24 10:04:09 +0530

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
-from distutils.core import setup
 from setuptools import setup, find_packages
 
 setup(
     name='Broadcaster',
-    version='1.0',
+    version='1.0.0',
     author='Gorbin, Niyas, Vinayak',
     author_email='broadcaster.thelycaeum@gmail.com',
     packages=find_packages(exclude=[]),
-    scripts=['broadcaster.py','README.md'],
+    scripts=['broadcast'],
     url='https://github.com/niyaspavil/Broadcaster',
     description='Broadcaster helps to post messages to different networking entities',
     long_description=open('README.md').read(),
     install_requires=[
+	"Mail==2.1.0",
         "argparse==1.2.1",
-        "tweepy==2.3.0",
-	"termcolor",
-        ],
+	"termcolor==1.1.0",
+	"tweepy==2.3.0",
+	"xmlrpclib==1.0.1"
+	
+       ],
 )
