a8b999679339586e46f4c0ffa30b6da838552880
gorbinphilip <gorbinphilip@gmail.com>
2014-10-21 14:05:55 +0530

    fix: test of datastore_connector

--- broadcaster/test/test_datastore_connector.py
+++ broadcaster/test/test_datastore_connector.py
@@ -1,15 +1,15 @@
 from ..datastore_connector import DataStore
+import pytest
 
-ds=None
-data=None
+ds=None 
 
 def test_init():
     global ds
-    ds=DataStore("user")
-
-def test_pull():
-    global data
-    data=ds.pull()
-
-def test_push():
-    ds.push(data)
+    with pytest.raises(NotImplementedError):
+       ds=DataStore("user")
+ 
+def test_methods():
+    with pytest.raises(AttributeError):
+        ds.pull()
+    with pytest.raises(AttributeError):
+        ds.push("unknown_data")
