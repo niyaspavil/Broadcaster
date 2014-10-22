f08f4566b807e93f7a319653f7a56cb9f02868c8
gorbinphilip <gorbinphilip@gmail.com>
2014-10-22 13:57:04 +0530

    implemented conf file parser in file_store_connector

--- broadcaster/file_store_connector.py
+++ broadcaster/file_store_connector.py
@@ -1,4 +1,32 @@
 from .datastore_connector import DataStore as ds
+import ConfigParser
+import os
+
+__private_home__=os.path.expanduser("~")+os.path.sep+".Broadcaster"
+__data_file__=__private_home__+os.path.sep+"data.ini"
+
+
+def get_data_from_file(file):
+    """returns the ConfigParser object with contents of file"""
+    if check_file(file):
+        r_data=ConfigParser.ConfigParser()
+        r_data.read(file)
+        return r_data
+    else:
+        raise Exception
+
+def check_file(file):
+    """checks whether file is already present, if not one is created."""
+    try:
+        path=os.path.dirname(file)
+        if not os.path.isfile(file):
+            if not os.path.exists(path):
+                os.makedirs(path)
+            new_f=open(file,"w")
+            new_f.close()
+        return True
+    except Exception:
+        return False
 
 class DataStore(ds):
     """provides the persistent data storage on a single local file"""
@@ -14,3 +42,5 @@ class DataStore(ds):
         """saves the data object passed to the datastore. this will replace the old data. returns True if succeeded"""
         pass
 
+
+__data_raw__=get_data_from_file(__data_file__)
--- broadcaster/test/test_file_store_connector.py
+++ broadcaster/test/test_file_store_connector.py
@@ -1,4 +1,5 @@
 from .. import file_store_connector as fsc
+import shutil, os, ConfigParser
 
 ds=None
 data=None
@@ -13,3 +14,14 @@ def test_pull():
 
 def test_push():
     ds.push(data)
+
+def test_check_file():
+    assert fsc.check_file("/tmp/test/test_file")==True
+    assert fsc.check_file("test")==False
+    shutil.rmtree("/tmp/test")
+
+def test_get_data_from_file():
+    data_r=fsc.get_data_from_file(os.path.dirname(__file__)+os.path.sep+"test_data"+os.path.sep+"data.ini")
+    assert isinstance(data_r, ConfigParser.ConfigParser)==True
+    assert data_r.get("neo","user")=="neo"
+    assert data_r.get("neo","timestamp")=="999"
